e1ededa1407c85f5e252bea7221ca16509cbdb9b
Diana Vasquez <divas16@gmail.com>
2021-02-25 07:26:51 -0500

    agrega ruta y componente de detalle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details', component: BookDetailsComponent },
+  { path: '',   redirectTo: '/details', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
